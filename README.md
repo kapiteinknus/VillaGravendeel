cb57c1cae7ff9e71e0f948b21badc0cb6e2dac33
Robert Spaan <robert.spaan@cavero.nl>
2015-03-11 15:47:56 +0100

    WebContent en README.md toegevoegd

---
+++ README.md
@@ -0,0 +1,1 @@
+# VillaGravendeel 
---
+++ WebContent/js/javascript.js
@@ -0,0 +1,14 @@
+//function initialize() {
+//	var mapCanvas = document.getElementById('map-canvas');
+//	var mapOptions = {
+//			center: new google.maps.LatLng(12.60441, -70.05152),
+//			zoom: 14,
+//			mapTypeId: google.maps.MapTypeId.ROADMAP
+//	}
+//    var map = new google.maps.Map(mapCanvas, mapOptions)
+//}
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+$(function() {
+	$( "#tabs" ).tabs();
+});
